24697bbd3803262e7880f0b18ad1e4ca64763bcd
otti95 <ottingerflorian@gmail.com>
2025-10-20 15:51:32 +0200

    Delete run.sh

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# Warten, bis USB-Gerät verfügbar ist
-while [ ! -e /dev/ttyUSB0 ]; do
-  echo "Warte auf Arduino..."
-  sleep 2
-done
-
-echo "Starte Dashboard..."
-python3 /data/send_dashboard_data.py
